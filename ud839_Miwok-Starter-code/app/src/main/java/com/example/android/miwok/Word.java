3f384670c280224db12cd6253b3769ae57bf79d2
saurabh deshpande <saurabh.deshpande@gocanvas.com>
2018-07-16 18:41:08 -0400

    Add images to the numbers,colors and family activity. Phrases activity should throw an error but is working fine.Note: will work on the phrases activity image view vsetvisibile part on next commit

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
@@ -4,12 +4,33 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
 
-    //constructor
+
+
+    /**
+     * A word constructor with english word and miwok word- specially for phrases activity
+     * @param defaultTranslation is the word in a language that the user wants
+     * @param miwokTranslation is the word in the Miwok language
+     */
     public Word(String defaultTranslation,String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+
+    }
+
+
+    /**
+     * Another Word constructor for including image to the custom adaptor- for colors, numbers and family activity
+     * @param defaultTranslation is the word in a language that the user wants
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image assets
+     */
+    public Word(String defaultTranslation,String miwokTranslation,int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     //get miwok translation of the word
@@ -21,4 +42,7 @@ public class Word {
     public String getmDefaultTranslation(){
         return mDefaultTranslation;
     }
+
+    //Get image resource ID
+    public int getImageResourceID(){return  mImageResourceId; }
 }
