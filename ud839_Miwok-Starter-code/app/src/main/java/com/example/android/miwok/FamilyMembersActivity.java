7f0d9b098db6971da8cec2a65df3974a9444fcfa
d27saurabh <d27saurabh@gmail.com>
2018-07-21 14:12:49 -0400

    Miwok: Visual Polish

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -26,7 +26,7 @@ public class FamilyMembersActivity extends AppCompatActivity {
         words.add(new Word("grandfather","paapa",R.drawable.family_grandfather));
 
 
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_family);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
 
+    /**Resource ID for teh background color for this list of words*/
+    private int mColorResourceId;
+
     /**
      *This is our own custom constructor (it doesn't mirror a superclass constructor).
      * The context is used to inflate the layout file, and the list is the data we want
@@ -23,12 +27,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
      * @param context current activity
      * @param words is the list of word to be displayed
      */
-    public WordAdapter(Activity context, ArrayList<Word> words){
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId ){
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context,0,words);
+        mColorResourceId = colorResourceId;
     }
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
@@ -73,8 +78,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         }
 
+        //Set theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        //Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(),mColorResourceId);
+        //Set the background container of the text container View
+        textContainer.setBackgroundColor(color);
 
-
+        //Return teh whole lisy item layout(conatining 2 textViews) so that it can be shown in the listView
         return listItemView;
 
     }
