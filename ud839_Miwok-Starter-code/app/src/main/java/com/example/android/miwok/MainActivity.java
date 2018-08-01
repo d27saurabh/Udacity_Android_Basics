f9f02f221074c0766ac693b4904b3f303889ff19
Saurabh Deshpande <d27saurabh@gmail.com>
2018-08-01 15:03:42 -0400

    Miwok: Final touches(Tab styled etc.)

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -36,13 +36,18 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this,getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
 
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
         tabLayout.setupWithViewPager(viewPager);
 
     }
