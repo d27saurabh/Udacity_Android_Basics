1a3de2c1a73e1e8fbe5f6d1e5ee01eca4377e9b1
Saurabh Deshpande <d27saurabh@gmail.com>
2018-08-03 11:38:49 -0400

    QuakeReport:Visual Changes and Earthquake Intent

--- ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -6,17 +6,21 @@ public class Earthquake {
     private String mLocation;
     private long mTimeInMilliseconds;
 
+    /** Website URL of the earthquake */
+    private String mUrl;
+
     /**
      * Constructor
      * @param magnitude is the magnitude of earthquake
      * @param location is the city location of the earthquake
      * @param date is the date of the earthquake happened
      */
-    public Earthquake(double magnitude, String location, Long date){
+    public Earthquake(double magnitude, String location, Long date, String url){
 
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = date;
+        mUrl = url;
     }
 
     public double getmMagnitude() {
@@ -29,5 +33,12 @@ public class Earthquake {
 
     public Long getmTimeInMilliseconds() {
         return mTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
     }
 }
